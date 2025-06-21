eca8d898a1a306c7897b8939c217e791d88a9dad
andregcaires <andregcaires@outlook.com>
2025-06-21 18:10:12 -0300

    comments adjustment

--- setup.sh
+++ setup.sh
@@ -6,15 +6,15 @@ install_os_specific_apps() {
         sudo apt update
         sudo apt install curl git -y
         
-        # for vscode
+        ## vscode
         sudo apt-get install wget gpg
-	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-	rm -f packages.microsoft.gpg
-	sudo apt install apt-transport-https -y
-	sudo apt update
-	sudo apt install code -y
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+        rm -f packages.microsoft.gpg
+        sudo apt install apt-transport-https -y
+        sudo apt update
+        sudo apt install code -y
 
     fi
 }
@@ -22,14 +22,14 @@ install_os_specific_apps() {
 install_through_snap() {
     if ! command -v snap --version >/dev/null 2>&1
     then
-        echo "snap not found, proceeding installation with apt"
+        echo "snap not found"
     else
         echo "snap was found"
     fi
 }
 
 install_through_online_scripts() {
-    ## SDKMAN for Java    
+    ## SDKMAN  
     if ! command -v sdkman version >/dev/null 2>&1
     then
     	curl -s "https://get.sdkman.io" | bash
@@ -38,7 +38,7 @@ install_through_online_scripts() {
         echo "sdkman was found"
     fi
 
-    ## FNM for Node.js
+    ## FNM 
     if ! command -v fnm --version >/dev/null 2>&1
     then
     	curl -fsSL https://fnm.vercel.app/install | bash ## deu ruim

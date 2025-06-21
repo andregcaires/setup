59c10a8d3ab108ce8d02ef84481cdd29ed79804d
andregcaires <andregcaires@outlook.com>
2025-06-21 18:03:44 -0300

    fix curl and vscode

--- setup.sh
+++ setup.sh
@@ -4,7 +4,18 @@ install_os_specific_apps() {
     if [ -f "/etc/debian_version" ]; then
         echo "'Debian' distro..."
         sudo apt update
-        sudo apt install git -y
+        sudo apt install curl git -y
+        
+        # for vscode
+        sudo apt-get install wget gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+	sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+	echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+	rm -f packages.microsoft.gpg
+	sudo apt install apt-transport-https -y
+	sudo apt update
+	sudo apt install code -y
+
     fi
 }
 
@@ -12,11 +23,8 @@ install_through_snap() {
     if ! command -v snap --version >/dev/null 2>&1
     then
         echo "snap not found, proceeding installation with apt"
-        # TODO 
     else
         echo "snap was found"
-        sudo snap install curl
-        sudo snap install code --classic
     fi
 }
 
@@ -39,7 +47,12 @@ install_through_online_scripts() {
     fi
     
     ## Rust
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    if ! command -v rustup --version >/dev/null 2>&1
+    then
+    	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    else
+        echo "rustup was found"
+    fi
 }
 
 echo "Starting setup..."

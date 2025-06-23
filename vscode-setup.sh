82aeff7adc9a65fafcd236cf959615bcc0b87e75
andregcaires <andregcaires@outlook.com>
2025-06-23 16:09:48 -0300

    add common extensions to profiles

--- vscode-setup.sh
+++ vscode-setup.sh
@@ -1,33 +1,69 @@
 #!/bin/bash
 
-code --install-extension pomdtr.excalidraw-editor
-code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code --install-extension figma.figma-vscode-extension
-code --install-extension alefragnani.project-manager
-code --install-extension eamodio.gitlens
+install_common_extensions() {
+    extension_list=(
+        pomdtr.excalidraw-editor 
+        ms-vscode-remote.vscode-remote-extensionpack 
+        figma.figma-vscode-extension 
+        alefragnani.project-manager 
+        eamodio.gitlens
+    )
+    if [ $# -eq 0 ]; then
+        echo "Instaling common extensions"
+        for extension in "${extension_list[@]}"; do
+            code --install-extension $extension
+        done
+    else 
+        echo "Instaling common extensions for $1 profile"
+        for extension in "${extension_list[@]}"; do
+            code --install-extension $extension --profile $1
+        done
+    fi
+}
 
 # Java / Spring 
 install_java_extensions() {
-    code --profile Java
-    extension_list=("vscjava.vscode-java-pack" "vmware.vscode-boot-dev-pack" "eamodio.gitlens" "ms-vscode-remote.vscode-remote-extensionpack" "visualstudioexptteam.vscodeintellicode")
-    for extension in ${extension_list[@]}; do
-        code --install-extension $extension --profile Java
+    CURRENT_PROFILE=Java
+    code --profile $CURRENT_PROFILE
+    
+    echo "Instaling ${CURRENT_PROFILE} extensions"
+    extension_list=(
+        "vscjava.vscode-java-pack" 
+        "vmware.vscode-boot-dev-pack" 
+        "visualstudioexptteam.vscodeintellicode"
+    )
+    for extension in "${extension_list[@]}"; do
+        code --install-extension $extension --profile $CURRENT_PROFILE
     done
+
+    install_common_extensions $CURRENT_PROFILE
 }
 
 # Python
 install_python_extensions() {
-    code --profile Python
-    extension_list=("ms-vscode-remote.vscode-remote-extensionpack" "visualstudioexptteam.vscodeintellicode-completions" "ms-python.python" "ms-python.debugpy" "ms-python.vscode-pylance" "visualstudioexptteam.vscodeintellicode" "eamodio.gitlens")
-    for extension in ${extension_list[@]}; do
-        code --install-extension $extension --profile Python
+    CURRENT_PROFILE=Python
+    code --profile $CURRENT_PROFILE
+    
+    echo "Instaling ${CURRENT_PROFILE} extensions"
+    extension_list=(
+        "visualstudioexptteam.vscodeintellicode-completions" 
+        "ms-python.python" 
+        "ms-python.debugpy" 
+        "ms-python.vscode-pylance" 
+        "visualstudioexptteam.vscodeintellicode"
+    )
+    for extension in "${extension_list[@]}"; do
+        code --install-extension $extension --profile $CURRENT_PROFILE
     done
+
+    install_common_extensions $CURRENT_PROFILE
 }
 
-if ! command -v code --version >/dev/null 2>&1
+if ! command -v code >/dev/null 2>&1
 then
     echo "vscode was not found"
 else
+    install_common_extensions
     install_java_extensions
     install_python_extensions
 fi

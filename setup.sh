f13b9d8a7f68ce3505187296f573523409526ca3
andregcaires <andregcaires@outlook.com>
2025-06-23 16:30:23 -0300

    add ms edge install via apt

--- setup.sh
+++ setup.sh
@@ -26,6 +26,19 @@ install_via_apt() {
         echo "vscode was found"
     fi
 
+    ## microsoft edge
+    if ! command -v microsoft-edge-stable >/dev/null 2>&1
+    then
+        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+        sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+        sudo rm microsoft.gpg
+
+        sudo apt update && sudo apt install microsoft-edge-stable
+    else
+        echo "microsoft-edge-stable was found"
+    fi
+
 }
 
 install_via_pacman() {

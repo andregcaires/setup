d87e3355a48952c9b82144b3ed0978a0629b04d0
andregcaires <andregcaires@outlook.com>
2025-06-26 14:35:29 -0300

    add wget and gpg to common packages

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
-package_list=("sudo" "curl" "git" "zip" "unzip")
+package_list=("sudo" "curl" "git" "zip" "unzip" "wget" "gpg")
 
 install_via_apt() {
     echo "installing via apt"
@@ -14,7 +13,6 @@ install_via_apt() {
     ## vscode
     if ! command -v code >/dev/null 2>&1
     then
-        sudo apt-get install wget gpg
         wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
         sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
         echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null

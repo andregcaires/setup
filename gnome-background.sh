ad3e506ea9cb0920a387912fdada2e72ad54deb6
andregcaires <andregcaires@outlook.com>
2025-07-08 08:50:38 -0300

    add wallpaper tweak

---
+++ gnome-background.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.background picture-uri ""
+gsettings set org.gnome.desktop.background picture-uri-dark ""
+
+gsettings set org.gnome.desktop.background primary-color '#000000'

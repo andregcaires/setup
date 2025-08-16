e4b468f40a67324cc54715941b9a18dcdb7ecd5c
andregcaires <andregcaires@outlook.com>
2025-08-16 19:02:50 -0300

    add flatpak, go, separate ppa option

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## TODO
+# decide between VS Code and VS Codium
+# add some dynamic versioning in Go installation
+
+set -e
+
 package_list=("sudo" "curl" "git" "zip" "unzip" "wget" "gpg" "podman")
 
 install_via_apt() {
@@ -9,53 +15,21 @@ install_via_apt() {
     for package in ${package_list[@]}; do
         sudo apt install $package -y
     done
-    
-    ## vscode
-    if ! command -v code >/dev/null 2>&1
-    then
-        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-        rm -f packages.microsoft.gpg
-        sudo apt install apt-transport-https -y
-        sudo apt update
-        sudo apt install code -y
-    else
-        echo "vscode was found"
-    fi
 
-    ## microsoft edge
-    if ! command -v microsoft-edge-stable >/dev/null 2>&1
-    then
-        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-        sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-        sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
-        sudo rm microsoft.gpg
+    ## VS Codium
+    # if ! command -v codium >/dev/null 2>&1
+    # then
+    #     wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+    #         | gpg --dearmor \
+    #         | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 
-        sudo apt update && sudo apt install microsoft-edge-stable
-    else
-        echo "microsoft-edge-stable was found"
-    fi
+    #     echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+    #         | sudo tee /etc/apt/sources.list.d/vscodium.sources
 
-    ## web app manager 
-    if ! command -v webapp-manager >/dev/null 2>&1
-    then
-        sudo add-apt-repository ppa:kelebek333/mint-tools
-        sudo apt update && sudo apt install python3-gi-cairo webapp-manager
-    else
-        echo "webapp-manager was found"
-    fi
-
-    ## google chrome
-    if ! command -v google-chrome-stable >/dev/null 2>&1
-    then
-        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-        sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-        sudo apt-get update
-        sudo apt-get install google-chrome-stable
-    else
-        echo "google-chrome-stable was found"
-    fi
+    #     sudo apt update && sudo apt install codium
+    # else
+    #     echo "vscodium was found"
+    # fi
 
     ## python 
     if ! command -v python3 >/dev/null 2>&1
@@ -93,16 +67,65 @@ install_os_specific_apps() {
     fi
 }
 
-install_through_online_scripts() {
-    ## SDKMAN  
-    if ! command -v sdkman version >/dev/null 2>&1
+install_through_ppas() {
+    ## google chrome
+    if ! command -v google-chrome-stable >/dev/null 2>&1
     then
-    	curl -s "https://get.sdkman.io" | bash
-        source "$HOME/.sdkman/bin/sdkman-init.sh"
+        wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+        sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+        sudo apt-get update
+        sudo apt-get install google-chrome-stable
     else
-        echo "sdkman was found"
+        echo "google-chrome-stable was found"
     fi
 
+    ## microsoft edge
+    if ! command -v microsoft-edge-stable >/dev/null 2>&1
+    then
+        curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+        sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+        sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-dev.list'
+        sudo rm microsoft.gpg
+
+        sudo apt update && sudo apt install microsoft-edge-stable
+    else
+        echo "microsoft-edge-stable was found"
+    fi
+
+    ## vscode
+    if ! command -v code >/dev/null 2>&1
+    then
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+        echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+        rm -f packages.microsoft.gpg
+        sudo apt install apt-transport-https -y
+        sudo apt update
+        sudo apt install code -y
+    else
+        echo "code was found"
+    fi
+
+    ## docker
+    if ! command -v docker >/dev/null 2>&1
+    then
+        sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+        sudo apt update
+        sudo apt install docker-ce docker-ce-cli containerd.io -y
+
+        sudo groupadd docker
+        sudo usermod -aG docker $USER
+        newgrp docker # To activate the group change without logging out and in
+    else
+        echo "docker was found"
+    fi
+}
+
+install_external_tools() {
     ## FNM 
     if ! command -v fnm --version >/dev/null 2>&1
     then
@@ -121,8 +144,54 @@ install_through_online_scripts() {
         echo "rustup was found"
     fi
 
+    ## Go
+    if ! command -v go --version >/dev/null 2>&1
+    then
+        curl -LO https://go.dev/dl/go1.25.0.linux-amd64.tar.gz
+
+        sudo tar -C /usr/local -xzf go1.25.0.linux-amd64.tar.gz
+
+        # configuring env var
+        echo -e "export PATH=\$PATH:/usr/local/go/bin\nexport GOPATH=\$HOME/go\nexport PATH=\$PATH:\$GOPATH/bin" | sudo tee /etc/profile.d/go.sh > /dev/null
+
+        sudo chmod +x /etc/profile.d/go.sh
+        cat /etc/profile.d/go.sh >> "$HOME/.bashrc"
+
+        # clean up
+        rm -f go1.25.0.linux-amd64.tar.gz
+        else
+            echo "go was found"
+    fi 
+
+    ## SDKMAN  
+    if ! command -v sdkman version >/dev/null 2>&1
+    then
+    	curl -s "https://get.sdkman.io" | bash
+        source "$HOME/.sdkman/bin/sdkman-init.sh"
+    else
+        echo "sdkman was found"
+    fi
+
+    source $HOME/.bashrc
+
     # VS Code extensions 
     #curl -fsSL https://raw.githubusercontent.com/andregcaires/setup/refs/heads/master/vscode-setup.sh | bash 
+}
+
+install_flatpak() {
+    if ! command -v flatpak --version >/dev/null 2>&1
+    then
+        echo "flatpak not found, must install and reboot afterwards"
+        sudo apt install flatpak
+        flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    else
+        echo "flatpak already installed"
+    fi
+}
+
+install_flatpak_apps() {
+
+    flatpak install com.spotify.Client com.getpostman.Postman com.opera.Opera com.microsoft.Edge org.chromium.Chromium -y
 }
 
 show_title() {
@@ -142,10 +211,12 @@ show_help() {
     echo "Use: source setup.sh [options]"
     echo
     echo "Available options:"
-    echo "  -a | --all          Install both external and package manager software"
-    echo "  -p | --package      Install software in the current package manager"
+    echo "  -a | --all          Install both external and package manager software (same as -b -e)"
+    echo "  -b | --basic        Install software in the current package manager"
     echo "                          Currently supported package managers: apt, pacman"
-    echo "  -e | --external     Install external packages"
+    echo "  -e | --external     Install external packages (FNM, SDKMAN, Rustup, Go)"
+    echo "  -f | --flatpak      Install Flatpak and some Flatpaks"
+    echo "  -p | --ppa          Install external packages through PPAs"
     echo "  -h | --help         Show help menu (you're here)"
     echo
 }
@@ -155,24 +226,31 @@ show_title
 if [ $# -eq 0 ]
 then
     show_menu
-elif [ "$#" -gt 1 ]; then
-    echo "Please use only one option"
-    show_help
+# elif [ "$#" -gt 1 ]; then
+#     echo "Please use only one option"
+#     show_help
 else
     for arg in "$@"; do
         case $arg in
             -a|--all)
             install_os_specific_apps
-            install_through_online_scripts
+            install_external_tools
             ;;
             -h|--help)
             show_help
             ;;
-            -p|--package)
+            -b|--basic)
             install_os_specific_apps
             ;;
+            -f|--flatpak)
+            install_flatpak
+            install_flatpak_apps
+            ;;
             -e|--external)
-            install_through_online_scripts
+            install_external_tools
+            ;;
+            -p|--ppa)
+            install_through_ppas
             ;;
             *)
             echo "Invalid argument: $arg"

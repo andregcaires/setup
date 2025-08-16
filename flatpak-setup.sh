eb59f0a433767f4ce873365c31404cfb226a1104
andregcaires <andregcaires@outlook.com>
2025-08-16 18:21:51 -0300

    add more browsers

--- flatpak-setup.sh
+++ flatpak-setup.sh
@@ -13,7 +13,7 @@ install_flatpak() {
 
 install_flatpak_apps() {
 
-    flatpak install com.spotify.Client com.getpostman.Postman com.opera.Opera -y
+    flatpak install com.spotify.Client com.getpostman.Postman com.opera.Opera com.microsoft.Edge org.chromium.Chromium -y
 }
 
 install_flatpak

2a80313e75209fc46208445e5f2fa6a48f1179fa
andregcaires <andregcaires@outlook.com>
2025-08-18 18:45:33 -0300

    codium available

--- setup.sh
+++ setup.sh
@@ -17,19 +17,19 @@ install_via_apt() {
     done
 
     ## VS Codium
-    # if ! command -v codium >/dev/null 2>&1
-    # then
-    #     wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
-    #         | gpg --dearmor \
-    #         | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+    if ! command -v codium >/dev/null 2>&1
+    then
+        wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+            | gpg --dearmor \
+            | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 
-    #     echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
-    #         | sudo tee /etc/apt/sources.list.d/vscodium.sources
+        echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
+            | sudo tee /etc/apt/sources.list.d/vscodium.sources
 
-    #     sudo apt update && sudo apt install codium
-    # else
-    #     echo "vscodium was found"
-    # fi
+        sudo apt update && sudo apt install codium
+    else
+        echo "vscodium was found"
+    fi
 
     ## python 
     if ! command -v python3 >/dev/null 2>&1
